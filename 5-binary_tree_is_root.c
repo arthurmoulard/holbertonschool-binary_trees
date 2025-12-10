cd928666ba7656415c1823f82bd8d9018818d83f
arthurmoulard <arthurmoulardperso@gmail.com>
2025-12-10 10:29:40 +0000

    Fix Last Betty Doc

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_is_root -checks if node is root
- * @node: node is binary leaf
- * Return 1 if node is a root
+ * binary_tree_is_root - function that checks if a node is a root
+ * @node: nodes is binary trees
+ * Return: 1 if node is a leaf, otherwise 0
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_inorder - traverses a tree in in-order calling func
+ * @tree: const pointer to root node to begin traversal at
+ * @func: function to call on tree node's data
+ */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
