82cbbd5958e0f35c85dd06d07e11f79e1c90a312
arthurmoulard <arthurmoulardperso@gmail.com>
2025-12-10 11:06:01 +0000

    Fix last Betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - a function that count the height of the tree
- * @size_t: the size of each node side
- * Return: the height of the tree
+ * binary_tree_height - gets the height of a binary tree
+ * @tree: root node to draw height from for tree
+ *
+ * Return: size_t representing height, 0 on failure or NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
