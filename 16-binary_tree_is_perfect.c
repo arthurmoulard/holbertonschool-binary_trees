92b4c363a37d691b58e0b6501c1aba0d3836812c
Denis12306 <denis.gavaud@gmail.com>
2025-12-10 10:52:43 +0100

    ModifFile

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -46,6 +46,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
     if (left_h != right_h)
         return (0);
+    if (left_h == right_h)
+        return (1);
 
     return (binary_tree_is_perfect(tree->left) &&
             binary_tree_is_perfect(tree->right));
