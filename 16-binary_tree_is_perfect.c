4659fadf5010786653cdb13d09089d740be6c5b2
Denis12306 <denis.gavaud@gmail.com>
2025-12-10 11:02:54 +0100

    Update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,33 +1,4 @@
 #include "binary_trees.h"
-
-/**
- * binary_tree_is_perfect - A function that checks if binary tree is perfect.
- * @tree: A pointer to the root of the tree.
- * Return: 1 if binary tree is perfect or 0 in otherwise.
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-    size_t h_left, h_right;
-
-    if (tree == NULL)
-        return (0);
-
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
-
-    if (tree->left == NULL || tree->right == NULL)
-        return (0);
-
-    h_left = binary_tree_height(tree->left);
-    h_right = binary_tree_height(tree->right);
-
-    if (h_left != h_right)
-        return (0);
-
-    return (binary_tree_is_perfect(tree->left) &&
-            binary_tree_is_perfect(tree->right));
-}
-
 /**
  * binary_tree_is_full - A function that checks if a binary tree is full.
  * @tree: A pointer to the root node of the tree.
@@ -51,6 +22,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
  * @tree: Pointer to root node to calculate balance factor.
  * Return: The balance factor of the tree.
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
@@ -80,4 +52,30 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
     return (left > right ? left : right);
 }
+/**
+ * binary_tree_is_perfect - A function that checks if binary tree is perfect.
+ * @tree: A pointer to the root of the tree.
+ * Return: 1 if binary tree is perfect or 0 in otherwise.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    size_t h_left, h_right;
 
+    if (tree == NULL)
+        return (0);
+
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+
+    if (tree->left == NULL || tree->right == NULL)
+        return (0);
+
+    h_left = binary_tree_height(tree->left);
+    h_right = binary_tree_height(tree->right);
+
+    if (h_left != h_right)
+        return (0);
+
+    return (binary_tree_is_perfect(tree->left) &&
+            binary_tree_is_perfect(tree->right));
+}
